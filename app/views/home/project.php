1ebc538e8942a5223eb940a113b03722ea5d941d
Irfan Thomi <irfanthomi@kelasduniaekasakti.co.id>
2023-03-19 14:27:24 +0700

    fix

--- app/views/home/project.php
+++ app/views/home/project.php
@@ -6,48 +6,51 @@
 
                 <?php
                 if (!$project) { ?>
-                    <h4 class="text-center">Project Tidak ditemukan</h4>
+                <h4 class="text-center">Project Tidak ditemukan</h4>
                 <?php }
                 foreach ($project as $p) : ?>
-                    <div class="col-md-3  d-flex flex-column  justify-content-center product-item mb-3">
-                        <div class="card-product h-100 ">
-                            <div class="product  ">
-                                <a href="<?= base_url('project_view') ?>/<?= $p['project_id'] ?>/<?= $p['project_name'] ?>">
-                                    <div class=" h-100">
-                                        <div class="project-slider h-100 swiper">
-                                            <div class="swiper-wrapper">
-                                                <?php
+                <div class="col-md-3  d-flex flex-column  justify-content-center product-item mb-3">
+                    <div class="card-product h-100 ">
+                        <div class="product  ">
+                            <a href="<?= base_url('project_view') ?>/<?= $p['project_id'] ?>/<?= $p['project_name'] ?>">
+                                <div class=" h-100">
+                                    <div class="project-slider h-100 swiper">
+                                        <div class="swiper-wrapper">
+                                            <?php
                                                 $images = explode(',', str_replace(" ", "", $p['images']));
                                                 $slide = (count($images) > 1) ? "swiper-slide" : "";
                                                 // print($p['images']);
                                                 foreach ($images as $image) : ?>
 
-                                                    <div class="<?= $slide ?>">
-                                                        <img src="<?= ($image) ?  base_url('') . 'rn/project/image/' . $image : base_url('') . "assets/img/static/blank-img.jpg" ?>" width="10" alt="<?= $image ?>">
-                                                    </div>
-                                                <?php
+                                            <div class="<?= $slide ?>">
+                                                <img class=" object-fit-cover"
+                                                    src="<?= ($image) ?  base_url('') . 'rn/project/image/' . $image : base_url('') . "assets/img/static/blank-img.jpg" ?>"
+                                                    width="10" alt="<?= $image ?>">
+                                            </div>
+                                            <?php
                                                 endforeach;
                                                 ?>
-                                            </div>
-                                            <div class="swiper-pagination"></div>
                                         </div>
-
+                                        <div class="swiper-pagination"></div>
                                     </div>
-                                </a>
+
+                                </div>
+                            </a>
 
 
-                            </div>
-                            <div class=" jus p-4">
-                                <a href="<?= base_url('project_view') ?>/<?= $p['project_id'] ?>/<?= $p['project_name'] ?>">
-
-                                    <div class=" align-items-center pt-2 ">
-                                        <small class=""><b><?= (str_word_count($p['project_name']) > 6 ? substr($p['project_name'], 0, 50) . "..." : $p['project_name'])  ?></b></small>
-                                    </div>
-                                </a>
-                            </div>
                         </div>
+                        <div class=" jus p-4">
+                            <a href="<?= base_url('project_view') ?>/<?= $p['project_id'] ?>/<?= $p['project_name'] ?>">
 
+                                <div class=" align-items-center pt-2 ">
+                                    <small
+                                        class=""><b><?= (str_word_count($p['project_name']) > 6 ? substr($p['project_name'], 0, 50) . "..." : $p['project_name'])  ?></b></small>
+                                </div>
+                            </a>
+                        </div>
                     </div>
+
+                </div>
 
                 <?php endforeach; ?>
 
