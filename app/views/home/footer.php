12dfadcfe3cfab3b91acad7eb93fe35a6c0105a2
Irfan Thomi <irfanthomi@kelasduniaekasakti.co.id>
2023-03-19 14:44:35 +0700

    sdfd

--- app/views/home/footer.php
+++ app/views/home/footer.php
@@ -1,6 +1,6 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
-    <div class="footer-newsletter">
+    <!-- <div class="footer-newsletter">
         <div class="container">
             <div class="row">
                 <div class=" d-flex align-items-center col-lg-6">
@@ -13,7 +13,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <div class="footer-top">
         <div class="container">
             <div class="row">
@@ -51,18 +51,21 @@
         </div>
     </div>
 </footer>
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 <script src="<?= base_url('') ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?= base_url('') ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
 <script src="<?= base_url('') ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
 <script src="<?= base_url('') ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
 <script src="<?= base_url('') ?>assets/vendor/waypoints/noframework.waypoints.js"></script>
 <script src="<?= base_url('') ?>assets/vendor/php-email-form/validate.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"
+    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script>
 <script src="<?= base_url('') ?>/assets/js/main1.js"></script>
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 <script>
-    AOS.init();
+AOS.init();
 </script>
